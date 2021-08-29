9d2253272448bbe09f793c139416225838129b7f
rinebergc <74808260+rinebergc@users.noreply.github.com>
2021-08-29 11:21:12 -0400

    Update installer_css to v2.
    Added additional script documentation, more fully explaining the purpose of the code.

--- installer_css.bat
+++ installer_css.bat
@@ -1,12 +1,41 @@
 @ECHO OFF
-SET client_dir=C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod
+
+REM MS Docs: REM records comments in a script, batch, or config.sys file.
+REM Echoing is set to off to limit limit console flooding.
+
+
+SET /p "driveLetter=Please enter the letter of the drive on which your Steam installation is located: "
+IF NOT DEFINED driveLetter SET "driveLetter=C"
+SET "clientDir=%driveLetter%:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod"
+
+REM The game library management system Steam can be installed on any lettered drive.
+REM Asking the user to specify their install location helps increase script versatility.
+REM If the user does not provide input the script will default to the user's C drive.
+
 
 powershell -command "If (Test-Path -Path %temp%\steamcmd\steamcmd.exe -PathType leaf) {} Else {If (Test-Path -Path %temp%\steamcmd.zip -PathType leaf) {Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd} Else {(New-Object Net.WebClient).DownloadFile('https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip','%temp%\steamcmd.zip'); Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd}}"
 
-START /b /wait %temp%\steamcmd\steamcmd.exe +login anonymous +force_install_dir %temp%\cstrike +app_update 232330 validate +quit
-START /b /wait ROBOCOPY "%temp%\cstrike\cstrike" "%client_dir%\addons\cstrike" "*.vpk"
+REM This script depends on access to the Steam Console Client or SteamCMD, a command-line version of the Steam client.
+REM If SteamCMD (unzipped) is located in the %temp% folder the script will continue to run.
+REM If SteamCMD (zipped) is located in the %temp% folder it will be extracted.
+REM If SteamCMD (zipped or unzipped) is not located in the %temp% folder it will be downloaded and extracted.
 
-(@ECHO "mountcfg" & @ECHO { & @ECHO "cstrike"  "%client_dir%\addons\cstrike" & @ECHO }) > "%client_dir%\cfg\mount.cfg"
-(@ECHO "gamedepotsystem" & @ECHO { & @ECHO "cstrike"  "1" & @ECHO }) > "%client_dir%\cfg\mountdepots.txt"
+
+START /b /wait %temp%\steamcmd\steamcmd.exe +login anonymous +force_install_dir %temp%\cstrike +app_update 232330 validate +quit
+START /b /wait ROBOCOPY "%temp%\cstrike\cstrike" "%clientDir%\addons\cstrike" "*.vpk"
+
+REM SteamCMD is called to download Counter Strike: Source resources to the user's Garry's Mod addons folder.
+REM Because the full game is not required only the necessary resource files will be copied.
+
+
+(@ECHO "mountcfg" & @ECHO { & @ECHO "cstrike"  "%clientDir%\addons\cstrike" & @ECHO }) > "%clientDir%\cfg\mount.cfg"
+(@ECHO "gamedepotsystem" & @ECHO { & @ECHO "cstrike"  "1" & @ECHO }) > "%clientDir%\cfg\mountdepots.txt"
+
+REM The script updates Garry's Mod configuration files to ensure the downloaded content is loaded.
+
 
 DEL /q %temp%\steamcmd.zip & RMDIR /s /q %temp%\steamcmd & RMDIR /s /q %temp%\cstrike
+
+REM MS Docs: The temp folder is not managed by Windows and it is the responsibility of the developer using it to clean up after themselves.
+REM This may no longer be true in Windows 10.
+
