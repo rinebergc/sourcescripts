5e3b4a997e082571bc59442be4457b0c7b9629f8
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2021-08-29 23:06:11 -0400

    Update css_content_client-1.0.bat

--- css_content_client-1.0.bat
+++ css_content_client-1.0.bat
@@ -2,6 +2,7 @@
 
 REM Echoing is set to off to limit console flooding.
 REM MS Docs: REM records comments in a script, batch, or config.sys file.
+REM
 REM More information on the ECHO command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/echo.
 REM More information on the REM command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/rem.
 
@@ -9,7 +10,8 @@ REM More information on the REM command is available at: https://docs.microsoft.
 TASKKILL /f /im hl2.exe
 ECHO To reduce potential issues while the script is running Garry's Mod (and by extension any hl2.exe based Source game) has been stopped.
 
-REM To reduce potential issues while the script is running Garry's Mod (and by extension any hl2.exe based Source game) has been stopped. 
+REM To reduce potential issues while the script is running Garry's Mod (and by extension any hl2.exe based Source game) has been stopped.
+REM
 REM More information on the TASKKILL command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/taskkill.
 
 
@@ -20,6 +22,7 @@ SET "clientDir=%driveLetter%:\Program Files (x86)\Steam\steamapps\common\GarrysM
 REM The game library management system Steam can be installed on any lettered drive.
 REM Asking the user to specify their install location helps increase script versatility.
 REM If the user does not provide input the script will default to the user's C drive.
+REM
 REM More information on the SET command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/set_1.
 REM More information on the IF command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/if.
 
@@ -30,6 +33,7 @@ REM This script requires the Steam Console Client or SteamCMD, a command-line ve
 REM If steamcmd.exe is located in %temp%\steamcmd: the script will continue to run.
 REM If steamcmd.zip is located in %temp%: steamcmd.exe will be extracted and the script will continue to run.
 REM If neither steamcmd.exe or steamcmd.zip are located in %temp%: steamcmd.zip will be downloaded, steamcmd.exe will be extracted and the script will continue to run.
+REM
 REM More information on POWERSHELL is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/powershell.
 REM More information on the Test-Path cmdlet is available at: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path?view=powershell-7.1.
 REM More information on the Expand-Archive cmdlet is available at: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/expand-archive?view=powershell-7.1.
@@ -43,6 +47,7 @@ REM Nesting these actions in START commands allows them to be done synchronously
 REM SteamCMD is called to anonymously connect to the Steam network and download Counter-Strike: Source Dedicated Server to %temp%.
 REM Because CS:SDS is based on CS:S it contains all the files necessary to enable CS:S support in Garry's Mod.
 REM To save disk space once CS:SDS is downloaded only the files required by Garry's Mod are copied, leaving the rest to be deleted during cleanup.
+REM
 REM More information on the START command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/start.
 REM More information on SteamCMD Commands is available at: https://developer.valvesoftware.com/wiki/Command_Line_Options#SteamCMD.
 REM More information on Steam Application IDs is available at: https://developer.valvesoftware.com/wiki/Steam_Application_IDs.
@@ -58,6 +63,7 @@ DEL /q %temp%\steamcmd.zip & RMDIR /s /q %temp%\steamcmd & RMDIR /s /q %temp%\cs
 
 REM MS Docs: The temp folder is not automatically emptied and cleanup is the responsibility of the developer using it.
 REM Note: Because of Storage sense in Windows 10 this may no longer be true and this could be considered legacy behavior.
+REM
 REM More information on the DEL command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/del.
 REM More information on the RMDIR command is available at: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/rmdir.
 REM More information on Storage sense is available at: https://support.microsoft.com/en-us/windows/manage-drive-space-with-storage-sense-654f6ada-7bfc-45e5-966b-e24aded96ad5.
