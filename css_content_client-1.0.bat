25dd17cf9e1c40757724c138a39459c76a9d78e7
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2021-11-17 00:46:59 -0500

    Update css_content_client-1.0.bat

--- css_content_client-1.0.bat
+++ css_content_client-1.0.bat
@@ -6,35 +6,34 @@ rem rem: https://docs.microsoft.com/en-us/windows-server/administration/windows-
 
 
 
-set /p "driveLetter=If Steam is installed on the C: drive press enter. Otherwise, enter the correct drive letter now: "
-if not defined driveLetter set "driveLetter=C"
-set "clientDir=driveLetter:\Program Files (x86)\Steam\steamapps\common\GarrysMod\garrysmod"
+set "folderPicker="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please select your GarrysMod client directory. Typically: This PC\Local Disk (C:)\Program Files (x86)\Steam\steamapps\common\GarrysMod.',0,0).self.path""
+for /f "usebackq delims=" %%I in (`powershell %folderPicker%`) do set "clientDir=%%I"
 
+rem for: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/for.
 rem set: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/set_1.
-rem if: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/if.
 
 
 
 wmic process where "ExecutablePath='%clientDir:\=\\%\\hl2.exe'" call terminate
 echo "Garry's Mod, if it was running, has been stopped. This will help prevent potential issues while this script runs."
 
-rem wmic: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/wmic.
 rem call: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/call.
+rem wmic: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/wmic.
 
 
 
 powershell -command "If (Test-Path -Path %temp%\steamcmd\steamcmd.exe -PathType leaf) {} Else {If (Test-Path -Path %temp%\steamcmd.zip -PathType leaf) {Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd} Else {(New-Object Net.WebClient).DownloadFile('https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip','%temp%\steamcmd.zip'); Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd}}"
 
 rem This script requires SteamCMD to function. Check for it in %temp% and %temp%\steamcmd. Continue if it's present. Otherwise, download it.
-rem powershell: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/powershell.
-rem test-path: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path?view=powershell-7.1.
 rem expand-archive: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/expand-archive?view=powershell-7.1.
+rem powershell: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/powershell.
 rem steamcmd: https://developer.valvesoftware.com/wiki/SteamCMD.
+rem test-path: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path?view=powershell-7.1.
 
 
 
 start /b /wait %temp%\steamcmd\steamcmd.exe +login anonymous +force_install_dir %temp%\cstrike +app_update 232330 validate +quit
-start /b /wait robocopy "%temp%\cstrike\cstrike" "%clientDir%\addons\cstrike" "*.vpk"
+start /b /wait robocopy "%temp%\cstrike\cstrike" "%clientDir%\garrysmod\addons\cstrike" "*.vpk"
 
 rem Download the Counter-Strike: Source Dedicated Server to %temp%\cstrike using steamCMD.
 rem The dedicated server includes the content, stored as .vpk's, required by Garry's Mod. To save disk space only these files are copied.
@@ -44,16 +43,16 @@ rem steam application ids: https://developer.valvesoftware.com/wiki/Steam_Applic
 
 
 
-rem Alternative: (echo "mountcfg" & ECHO { & echo "cstrike"  "%clientDir%\addons\cstrike" & echo }) > "%clientDir%\cfg\mount.cfg"
-> "%clientDir%\cfg\mount.cfg" (
+rem Alternative: (echo "mountcfg" & ECHO { & echo "cstrike"  "%clientDir%\garrysmod\addons\cstrike" & echo }) > "%clientDir%\garrysmod\cfg\mount.cfg"
+> "%clientDir%\garrysmod\cfg\mount.cfg" (
 	echo "mountcfg"
 	echo {
-	echo "cstrike"  "%clientDir%\addons\cstrike"
+	echo "cstrike"  "%clientDir%\garrysmod\addons\cstrike"
 	echo }
 )
 
-rem Alternative: (echo "gamedepotsystem" & echo { & echo "cstrike"  "1" & echo }) > "%clientDir%\cfg\mountdepots.txt"
-> "%clientDir%\cfg\mountdepots.txt" (
+rem Alternative: (echo "gamedepotsystem" & echo { & echo "cstrike"  "1" & echo }) > "%clientDir%\garrysmod\cfg\mountdepots.txt"
+> "%clientDir%\garrysmod\cfg\mountdepots.txt" (
 	echo "gamedepotsystem"
 	echo {
 	echo "cstrike"  "1"
