adb43e81df9923f9831bca0e85c27d1b3511a535
Carter Rineberg <74808260+rinebergc@users.noreply.github.com>
2021-11-17 11:31:02 -0500

    Update css_content_client-1.0.bat
    Improved readability and robustness.

--- css_content_client-1.0.bat
+++ css_content_client-1.0.bat
@@ -1,67 +1,58 @@
 @echo off
 
 rem Comments have been added to provide clarity and educational value where possible.
-rem echo: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/echo.
-rem rem: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/rem.
+rem For more information on Powershell cmdlets see: https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.1
+rem For more information on SteamCMD see: https://developer.valvesoftware.com/wiki/SteamCMD, https://developer.valvesoftware.com/wiki/Command_Line_Options#SteamCMD, and https://developer.valvesoftware.com/wiki/Steam_Application_IDs.
+rem For more information on Windows commands see: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/windows-commands
 
 
 
 set "folderPicker="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please select your GarrysMod client directory. Typically: This PC\Local Disk (C:)\Program Files (x86)\Steam\steamapps\common\GarrysMod.',0,0).self.path""
-for /f "usebackq delims=" %%I in (`powershell %folderPicker%`) do set "clientDir=%%I"
-
-rem for: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/for.
-rem set: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/set_1.
+for /f "usebackq delims=" %%I in (`powershell -command %folderPicker%`) do set "clientDir=%%I"
+rem Prompt the user to select their Garry's Mod directory rather than assume where it was installed.
 
 
 
-powershell -command "If (Test-Path -Path %temp%\steamcmd\steamcmd.exe -PathType leaf) {} Else {If (Test-Path -Path %temp%\steamcmd.zip -PathType leaf) {Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd} Else {(New-Object Net.WebClient).DownloadFile('https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip','%temp%\steamcmd.zip'); Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd}}"
-
-rem This script requires SteamCMD to function. Check for it in %temp% and %temp%\steamcmd. Continue if it's present. Otherwise, download it.
-rem expand-archive: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/expand-archive?view=powershell-7.1.
-rem powershell: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/powershell.
-rem steamcmd: https://developer.valvesoftware.com/wiki/SteamCMD.
-rem test-path: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path?view=powershell-7.1.
+set steamcmdCheck= ^
+if (Test-Path -Path %temp%\steamcmd\steamcmd.exe -PathType leaf) { ^
+} elseif (Test-Path -Path %temp%\steamcmd.zip -PathType leaf) { ^
+	Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd ^
+} elseif (Test-Path -Path %temp%\steamcmd.exe -PathType leaf) { ^
+	if (-not (Test-Path -Path %temp%\steamcmd -PathType container)) {New-Item -ItemType directory -Path %temp%\steamcmd} ^
+	Move-Item -Path %temp%\steamcmd.exe -Destination %temp%\steamcmd\steamcmd.exe ^
+} else { ^
+	(New-Object Net.WebClient).DownloadFile('https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip','%temp%\steamcmd.zip'); ^
+	Expand-Archive -LiteralPath %temp%\steamcmd.zip -DestinationPath %temp%\steamcmd ^
+}
+powershell -command "%steamcmdCheck%"
+rem This script requires SteamCMD to function. Check for it and continue if it's present. Otherwise, download it.
 
 
 
 start /b /wait %temp%\steamcmd\steamcmd.exe +login anonymous +force_install_dir %temp%\cstrike +app_update 232330 validate +quit
 start /b /wait robocopy "%temp%\cstrike\cstrike" "%clientDir%\garrysmod\addons\cstrike" "*.vpk"
-
 rem Download the Counter-Strike: Source Dedicated Server to %temp%\cstrike using steamCMD.
 rem The dedicated server includes the content, stored as .vpk's, required by Garry's Mod. To save disk space only these files are copied.
-rem start: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/start.
-rem steamcmd commands: https://developer.valvesoftware.com/wiki/Command_Line_Options#SteamCMD.
-rem steam application ids: https://developer.valvesoftware.com/wiki/Steam_Application_IDs.
 
 
 
 wmic process where "ExecutablePath='%clientDir:\=\\%\\hl2.exe'" call terminate
 echo "Garry's Mod, if it was running, has been stopped. This will help prevent potential issues while this script runs."
-
-rem call: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/call.
-rem wmic: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/wmic.
-
-
-
 > "%clientDir%\garrysmod\cfg\mount.cfg" (
 	echo "mountcfg"
 	echo {
 	echo "cstrike"  "%clientDir%\garrysmod\addons\cstrike"
 	echo }
 )
-
 > "%clientDir%\garrysmod\cfg\mountdepots.txt" (
 	echo "gamedepotsystem"
 	echo {
 	echo "cstrike"  "1"
 	echo }
 )
-
-rem Configure Garry's Mod to mount the newly downloaded content.
+rem Kill Garry's Mod, if it's running, and edit mount/mountdepots.cfg to mount the newly downloaded content.
 
 
 
 del /q %temp%\steamcmd.zip & rmdir /s /q %temp%\steamcmd & rmdir /s /q %temp%\cstrike
-
-rem del: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/del.
-rem rmdir: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/rmdir.
+rem Cleanup - Remove temporary files and directories.
